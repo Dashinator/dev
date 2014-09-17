0b865832517d468220cac1cbda72ced17cc0dbf8
Pieter <Pee_em_em@hotmail.com>
2014-09-18 00:50:51 +0200

    setup

---
+++ Assets/NetworkManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class NetworkManager : MonoBehaviour {
+    public GameObject playerPrefab;
+    private const string roomName = "RoomName";
+    private RoomInfo[] roomsList;
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+	}
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+    void OnReceivedRoomListUpdate()
+    {
+        roomsList = PhotonNetwork.GetRoomList();
+    }
+
+    void OnJoinedLobby()
+    {
+        //roomsList = PhotonNetwork.GetRoomList();
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        // Spawn player
+        PhotonNetwork.Instantiate(playerPrefab.name, Vector3.up * 5, Quaternion.identity, 0);
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Photon.MonoBehaviour
+{
+    public float speed              = 10f;
+    public float jumpspeed          = 200f;
+    private bool jump               = false;
+    public float mouseSensitivity = 5.0f;
+    
+    float verticalRotation = 0;
+    public float upDownRange = 60.0f;
+
+    void Start()
+    {
+        Screen.lockCursor = true;
+    }
+
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            InputMovement();
+            InputColorChange();
+        }
+    }
+
+    void InputMovement()
+    {
+        float rotLeftRight = Input.GetAxis("Mouse X") * mouseSensitivity;
+        transform.Rotate(0, rotLeftRight, 0);
+
+        verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+
+        /*if (Input.GetKey(KeyCode.W))
+            rigidbody.MovePosition(rigidbody.position + Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            rigidbody.MovePosition(rigidbody.position - Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            rigidbody.MovePosition(rigidbody.position + Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            rigidbody.MovePosition(rigidbody.position - Vector3.right * speed * Time.deltaTime);
+        */
+        float forwardSpeed = Input.GetAxis("Vertical") * speed;
+        float sideSpeed = Input.GetAxis("Horizontal") * speed;
+
+
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+            Screen.lockCursor = !Screen.lockCursor;
+
+    }
+
+
+    private void InputColorChange()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)));
+    }
+
+    [RPC]
+    void ChangeColorTo(Vector3 color)
+    {
+        renderer.material.color = new Color(color.x, color.y, color.z, 1f);
+
+        if (photonView.isMine)
+            photonView.RPC("ChangeColorTo", PhotonTargets.OthersBuffered, color);
+    }
+}
